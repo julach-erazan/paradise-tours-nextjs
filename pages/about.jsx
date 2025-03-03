0d7fd6b55690694c62f3c8a1f1470d8e1a8378bb
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-03-03 13:02:08 +0530

    Create about.jsx

---
+++ pages/about.jsx
@@ -0,0 +1,140 @@
+import Layouts from '@/src/layout/LayoutAbout'
+import React from 'react'
+
+
+
+
+
+export default function about() {
+  return (
+    <Layouts>
+
+
+      <div className="text-black">
+        <div className='py-5 lg:py-16'>
+          <p className='text-justify text-black text-sm md:text-xl '>Welcome to paradise, your ultimate gateway to the enchanting island of Sri Lanka! With a passion for travel and a commitment to excellence, we specialize in crafting unforgettable journeys that cater to your unique preferences and interests.</p>
+        </div>
+        <section id="our-story" className="mb-8 md:mb-16 ">
+          <div className='grid grid-cols-1 md:grid-cols-12 gap-8'>
+            <div className='col-span-1 my-auto md:col-span-3'>
+              <h1 className='text-xl py-2 bg-clip-text text-transparent bg-gradient-to-r from-lime-500  to-salmon-500  my-auto md:text-5xl font-bold uppercase'>
+                Our
+                Story
+              </h1>
+            </div>
+            <div className='col-span-1  md:col-span-9'>
+              <p className='text-justify'>Paradise was born out of a deep love for Sri Lankas rich culture, stunning landscapes, and warm hospitality. Our founders, avid travelers themselves, recognized the need for a travel agency that offers personalized, authentic experiences. Since our inception, we have dedicated ourselves to showcasing the best of Sri Lanka, ensuring that every traveler leaves with cherished memories and a longing to return.</p>
+
+            </div>
+          </div>
+        </section>
+
+        <section id="our-story" className="mb-5 md:mb-16">
+          <div className='grid grid-cols-1  md:grid-cols-12 gap-8'>
+            <div className='col-span-1 2xl:col-span-1  md:col-span-2'>
+              <div className='text-xl  bg-clip-text text-center flex md:grid   text-transparent bg-gradient-to-r from-lime-500 to-salmon-500 2xl:text-7xl   md:text-8xl font-bold uppercase'>
+              <p>w</p>
+              <p>h</p>
+              <p className='mb-10 px-auto'>y</p>
+              <p>c</p>
+              <p>h</p>
+              <p>o</p>
+              <p>o</p>
+              <p>s</p>
+              <p className='mb-10 px-auto'>e</p>
+              <p>u</p>
+              <p>s</p>
+              </div>
+            </div>
+            <div className='col-span-1 2xl:col-span-11   md:col-span-10'>
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-4 xl:gap-8'>
+              <div className= 'mb-5 xl:mb-10'>
+                <span className='text-2xl  font-semibold'> Personalized Itineraries:</span>
+                <p className='text-justify lg:text-[10px] xl:text-[16px] text-[10px]'>We recognize that each person is different in their preferences. Therefore, we address this by ourizing our tours to your exact wants and needs. Irrespective of whether you want to have adventures or relaxation, personalizing the travel schedule and packing it according to your individual preferences will be our responsibility so that your trip is in line with your desired experiences.</p>
+              </div>
+              
+              <img className='mb-5 w xl:mb-10 object-cover w-full h-48 xl:h-72 rounded-ss-[200px] rounded-ee-[200px] overflow-hidden' src="/About/a1.jpg" alt="" />
+              <img className='mb-5 xl:mb-10 object-cover w-full h-48 xl:h-72 rounded-ss-[200px] rounded-ee-[200px]  overflow-hidden' src="/About/a2.jpg" alt="" />
+              <div className='mb-5 xl:mb-10'>
+              <span className='text-2xl  font-semibold'>Expert Local Knowledge:</span>
+                <p className='text-justify lg:text-[10px] xl:text-[16px] text-[10px]'> Our team including local specialist who have well knowledge about the sought-after plot points of the country as well know the secret hideouts of Sri Lanka. Hearth & Soul is a local company providing you with non-touristic meaningful experience. See the world with a viewpoint of the locals and past a common path of a tourist.</p>
+              </div>
+            
+              <div className='mb-5 xl:mb-10'>
+              <span className='text-2xl  font-semibold'>Seamless Experience: </span>
+                <p className='text-justify lg:text-[10px] xl:text-[16px] text-[10px]'>Starting from the moment you reach out to us through the end of your trip, we manage everything, leaving you without concerns about the process and you being able to enjoy what you do. Our full-scale services begin with where you will sleep through how you will travel, explore the area and participate in the special activities that will make you comfortable and at ease.</p>
+              </div>
+              <img className='mb-5 xl:mb-10 object-cover w-full h-48 xl:h-72  rounded-ss-[200px] rounded-ee-[200px] overflow-hidden' src="/About/a3.jpg" alt="" />
+              <img className='mb-5 xl:mb-10 object-cover w-full h-48 xl:h-72 rounded-ss-[200px] rounded-ee-[200px] overflow-hidden' src="/About/a4.jpg" alt="" />
+              <div className='mb-5 xl:mb-10'>
+              <span className='text-2xl  font-semibold'>Sustainable Travel: </span>
+                <p className='text-justify lg:text-[10px] xl:text-[16px] text-[10px]'>Through our responsible tourism venture, we pledge to encourage activities that assure the welfare of the local communities and the well-being of the environment. Taproscapes will not only have a positive impact on environmental protection but also promote sustainable practices. Your decision to select Taproscapes will help support eco-friendly initiatives that conserve Sri-Lanka’s rich ecological diversity.</p>
+              </div>
+              <div className='mb-5 xl:mb-10'>
+              <span className='text-2xl  font-semibold'>  24/7 Support:</span>
+                <p className='text-justify lg:text-[10px] xl:text-[16px] text-[10px]'>Our Customer service team is always ready to help you even after the clock strikes midnight if you require support that will be provided without quarrel. Our exclusive commitment is to assure the confidence and security of our customers and to provide them with immediate assistance whenever its needed.</p>
+              </div>
+              <img className='mb-5 xl:mb-10 object-cover w-full h-48 xl:h-72 rounded-ss-[200px] rounded-ee-[200px] overflow-hidden' src="/About/a5.jpg" alt="" />
+              <img className='mb-5 xl:mb-10 object-cover w-full h-48 xl:h-72 rounded-ss-[200px] rounded-ee-[200px] overflow-hidden' src="/About/a6.jpg" alt="" />
+              <div className='mb-5 xl:mb-10'>
+              <span className='text-2xl  font-semibold'> Exclusive Partnerships: </span>
+                <p className='text-justify lg:text-[10px] xl:text-[16px] text-[10px]'>We have made the relationship with different hotels, restaurants, and activity providers our strength that provided you with exquisite deals and experience. Members will receive an exclusive access to special rates, select locations and variety of events that will be arranged only for our members.</p>
+              </div>
+              <div className='mb-5 xl:mb-10'>
+              <span className='text-2xl  font-semibold'> Authentic Cultural Experiences: </span>
+                <p className='text-justify lg:text-[10px] xl:text-[16px] text-[10px]'>Taproscapes provides you a unique space where you get a living connection with Sri Lanka’s deep culture. Culture shares opens its doors to our tour visitors; they interact directly with the local traditions by participating in ceremonies alongside our guides or by visiting craftsmen, which paves the way for a deeper experience and wider perception of the local culture.</p>
+              </div>
+              <img className='mb-5 xl:mb-10 object-cover w-full h-48 xl:h-72 rounded-ss-[200px] rounded-ee-[200px] overflow-hidden' src="/About/a8.jpg" alt="" />
+            </div>
+            </div>
+          </div>
+        </section>
+
+
+       
+
+
+        {/* <section id="our-team" className="mb-8 flex justify-center">
+          <div className="max-w-4xl w-full">
+            <h2 className="text-2xl font-bold mb-2 text-center">Our Team</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
+            
+              <div className="bg-gray-500 p-4 rounded-lg">
+                <h3 className="text-lg font-semibold mb-1">John Doe</h3>
+                <p className="text-sm">Founder and CEO</p>
+                <p className="mt-2">Description about John Doe...</p>
+              </div>
+              <div className="bg-gray-500 p-4 rounded-lg">
+                <h3 className="text-lg font-semibold mb-1">Jane Smith</h3>
+                <p className="text-sm">Head of Operations</p>
+                <p className="mt-2">Description about Jane Smith...</p>
+              </div>
+             
+            </div>
+          </div>
+        </section> */}
+
+        {/* <section id="why-choose" className="mb-8 items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2  items-center gap-4">
+            <div className="bg-none p-4 rounded-lg">
+              <h2 className="text-2xl font-bold mb-2">Why Choose LankaTravels?</h2>
+              <ul className="list-disc list-inside">
+                <li>Personalized Services: Describe how...</li>
+                <li>Local Expertise: Emphasize the...</li>
+                <li>Exceptional Customer Service: Highlight...</li>
+                <li>Sustainability and Responsibility: Showcase...</li>
+                <li>Value for Money: Illustrate how...</li>
+                <li>Customer Testimonials: Include quotes...</li>
+              </ul>
+            </div>
+            <div className="bg-none p-4 rounded-lg">
+              <img src='/colombo.jpg' alt='Colombo' width={500} height={500} />
+            </div>
+          </div>
+        </section> */}
+
+        {/* Additional sections can be added as needed */}
+      </div>
+    </Layouts>
+  )
+}
