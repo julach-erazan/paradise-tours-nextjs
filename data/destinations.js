30034214ab30bb2a8878ed3bfe8fb7229a73d3f8
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-03-03 12:56:32 +0530

    Create destinations.js

---
+++ data/destinations.js
@@ -0,0 +1,246 @@
+export const destinations = [
+  
+  {
+    id: '16',
+    name: 'Horton Plains',
+    description: 'The misty plains of Horton',
+    image: '/horton.jpg',
+    photos: ['/horton/hor1.jpg', '/horton/hor2.jpg'],
+    videos: ['/horton.mp4']
+},
+  
+
+  {
+      id: '2',
+      name: 'Sigiriya',
+      description: `The Lion’s Rock of Sigiriya`,
+      image: '/sigiriya2.jpg',
+      photos: ['/colombo.jpg', '/colombo.jpg'],
+      videos: ['/slV.mp4', '/slV.mp4', '/slV.mp4']
+  },
+  {
+      id: '3',
+      name: 'Galle',
+      description: 'The port of Galle',
+      image: '/galle4.jpg',
+      photos: ['/colombo.jpg', '/colombo.jpg'],
+      videos: ['https://youtu.be/coD9dUz5sW0?si=txFY1DMviLD0SO_R']
+  },
+  {
+      id: '4',
+      name: 'Ella',
+      description: 'The waterfall city of Ella',
+      image: '/ella4.jpg',
+      photos: ['/colombo.jpg', '/colombo.jpg'],
+      videos: ['https://www.youtube.com/watch?v=q0mbKsKG-ng&ab_channel=BeautifulDestinations']
+  },
+  {
+      id: '5',
+      name: 'Jaffna',
+      description: 'The city of marine life Jaffna',
+      image: '/jaffna.jpg',
+      photos: ['/colombo.jpg', '/colombo.jpg'],
+      videos: ['https://www.youtube.com/watch?v=q0mbKsKG-ng&ab_channel=BeautifulDestinations']
+  },
+  {
+      id: '6',
+      name: 'Trincomalee',
+      description: 'Trinco where wild animals often roam freely',
+      image: '/trinco.jpg',
+      photos: ['/colombo.jpg', '/colombo.jpg'],
+      videos: ['https://www.youtube.com/watch?v=q0mbKsKG-ng&ab_channel=BeautifulDestinations']
+  },
+  {
+      id: '7',
+      name: 'Nuwara Eliya',
+      description: 'City of nature',
+      image: '/tea.jpg',
+      photos: ['/colombo.jpg', '/colombo.jpg'],
+      videos: ['https://www.youtube.com/watch?v=q0mbKsKG-ng&ab_channel=BeautifulDestinations']
+  },
+  {
+      id: '8',
+      name: 'Polonnaruwa',
+      description: 'A kingdom full of ancient ruins',
+      image: '/pol3.jpg',
+      photos: ['/colombo.jpg', '/colombo.jpg'],
+      videos: ['https://www.youtube.com/watch?v=q0mbKsKG-ng&ab_channel=BeautifulDestinations']
+  },
+  {
+      id: '9',
+      name: 'Kandy',
+      description: 'The most beautiful city in Sri Lanka',
+      image: '/kandy.jpg',
+      photos: ['/colombo.jpg', '/colombo.jpg'],
+      videos: ['https://www.youtube.com/watch?v=q0mbKsKG-ng&ab_channel=BeautifulDestinations']
+  },
+  {
+      id: '10',
+      name: 'Unawatuna',
+      description: 'The beach paradise of Unawatuna',
+      image: '/unawatuna.jpg',
+      photos: ['/colombo.jpg', '/colombo.jpg'],
+      videos: ['https://www.youtube.com/watch?v=q0mbKsKG-ng&ab_channel=BeautifulDestinations']
+  },
+  {
+      id: '11',
+      name: 'Mirissa',
+      description: 'Whale watching capital of Sri Lanka',
+      image: '/Mirissa.jpg',
+      photos: ['/mirissa/mir1.jpg', '/mirissa/mir2.jpg'],
+      videos: ['/mirissa.mp4']
+  },
+  {
+      id: '12',
+      name: 'Anuradhapura',
+      description: 'The sacred city of Anuradhapura',
+      image: '/anuradhapura.jpg',
+      photos: ['/anuradhapura/anu1.jpg', '/anuradhapura/anu2.jpg'],
+      videos: ['/anuradhapura.mp4']
+  },
+  {
+      id: '13',
+      name: 'Bentota',
+      description: 'The beach town of Bentota',
+      image: '/bentota.jpg',
+      photos: ['/bentota/ben1.jpg', '/bentota/ben2.jpg'],
+      videos: ['/bentota.mp4']
+  },
+  {
+      id: '14',
+      name: 'Arugam Bay',
+      description: 'Surfer’s paradise at Arugam Bay',
+      image: '/arugam.jpg',
+      photos: ['/arugam/aru1.jpg', '/arugam/aru2.jpg'],
+      videos: ['/arugam.mp4']
+  },
+  {
+      id: '15',
+      name: 'Yala National Park',
+      description: 'The wildlife haven of Yala',
+      image: '/yala.jpg',
+      photos: ['/yala/yal1.jpg', '/yala/yal2.jpg'],
+      videos: ['/yala.mp4']
+  },
+
+  {
+      id: '17',
+      name: 'Udawalawe National Park',
+      description: 'Elephant sanctuary of Udawalawe',
+      image: '/udawalawe.jpg',
+      photos: ['/udawalawe/uda1.jpg', '/udawalawe/uda2.jpg'],
+      videos: ['/udawalawe.mp4']
+  },
+  {
+      id: '18',
+      name: 'Adam’s Peak',
+      description: 'The sacred mountain of Adam’s Peak',
+      image: '/adam.jpg',
+      photos: ['/adamspeak/adam1.jpg', '/adamspeak/adam2.jpg'],
+      videos: ['/adamspeak.mp4']
+  },
+  {
+      id: '19',
+      name: 'Dambulla Cave Temple',
+      description: 'The ancient caves of Dambulla',
+      image: '/dabulla.jpg',
+      photos: ['/dambulla/dam1.jpg', '/dambulla/dam2.jpg'],
+      videos: ['/dambulla.mp4']
+  },
+  {
+      id: '20',
+      name: 'Hikkaduwa',
+      description: 'The coral sanctuary of Hikkaduwa',
+      image: '/hikkaduwa.jpg',
+      photos: ['/hikkaduwa/hik1.jpg', '/hikkaduwa/hik2.jpg'],
+      videos: ['/hikkaduwa.mp4']
+  },
+  {
+      id: '21',
+      name: 'Kalpitiya',
+      description: 'The dolphin watching spot of Kalpitiya',
+      image: '/kalpitiya.jpg',
+      photos: ['/kalpitiya/kal1.jpg', '/kalpitiya/kal2.jpg'],
+      videos: ['/kalpitiya.mp4']
+  },
+  {
+      id: '22',
+      name: 'Negombo',
+      description: 'The lagoon town of Negombo',
+      image: '/negombo.jpg',
+      photos: ['/negombo/neg1.jpg', '/negombo/neg2.jpg'],
+      videos: ['/negombo.mp4']
+  },
+  {
+      id: '23',
+      name: 'Wilpattu National Park',
+      description: 'The oldest national park in Sri Lanka',
+      image: '/wilpattu.jpg',
+      photos: ['/wilpattu/wil1.jpg', '/wilpattu/wil2.jpg'],
+      videos: ['/wilpattu.mp4']
+  },
+  {
+      id: '24',
+      name: 'Mannar Island',
+      description: 'The remote beauty of Mannar Island',
+      image: '/mannar.jpg',
+      photos: ['/mannar/man1.jpg', '/mannar/man2.jpg'],
+      videos: ['/mannar.mp4']
+  },
+  {
+      id: '25',
+      name: 'Batticaloa',
+      description: 'The historical city of Batticaloa',
+      image: '/batticaloa.jpg',
+      photos: ['/batticaloa/bat1.jpg', '/batticaloa/bat2.jpg'],
+      videos: ['/batticaloa.mp4']
+  },
+  {
+      id: '26',
+      name: 'Ratnapura',
+      description: 'The city of gems, Ratnapura',
+      image: '/ratnapura.jpg',
+      photos: ['/ratnapura/rat1.jpg', '/ratnapura/rat2.jpg'],
+      videos: ['/ratnapura.mp4']
+  },
+  {
+      id: '27',
+      name: 'Sinharaja Forest Reserve',
+      description: 'The biodiversity hotspot of Sinharaja',
+      image: '/sinharaja.jpg',
+      photos: ['/sinharaja/sin1.jpg', '/sinharaja/sin2.jpg'],
+      videos: ['/sinharaja.mp4']
+  },
+  {
+      id: '28',
+      name: 'Kitulgala',
+      description: 'The adventure capital of Kitulgala',
+      image: '/kitulgala.jpg',
+      photos: ['/kitulgala/kit1.jpg', '/kitulgala/kit2.jpg'],
+      videos: ['/kitulgala.mp4']
+  },
+  {
+      id: '29',
+      name: 'Pinnawala Elephant Orphanage',
+      description: 'The sanctuary for orphaned elephants',
+      image: '/pinnawala.jpg',
+      photos: ['/pinnawala/pin1.jpg', '/pinnawala/pin2.jpg'],
+      videos: ['/pinnawala.mp4']
+  },
+  {
+      id: '30',
+      name: 'Pasikuda',
+      description: 'The tranquil beaches of Pasikuda',
+      image: '/pasikuda.jpg',
+      photos: ['/pasikuda/pas1.jpg', '/pasikuda/pas2.jpg'],
+      videos: ['/pasikuda.mp4']
+  },
+  {
+    id: '1',
+    name: 'Colombo',
+    description: 'The City of Lights',
+    image: '/colombo5.jpg',
+    photos: ['/colombo/col3.jpg', '/colombo/col2.jpg',"/colombo/col1.jpg", '/colombo/col4.jpg','/colombo/8.jpg','/colombo/7.jpg','/colombo/5.jpg','/colombo/col5.jpg'],
+    videos: ['/colombo.mp4', '/col2.mp4', ]
+  },
+];
